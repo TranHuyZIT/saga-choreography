b017e0533d11d1d2543da9a2bf4bad62fbafe58b
TranHuy2k2 <giahuytran2002@gmail.com>
2023-12-19 17:32:15 +0700

    chore: initalize project

---
+++ src/main/java/com/example/sagachoreography/SagaChoreographyApplication.java
@@ -0,0 +1,13 @@
+package com.example.sagachoreography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaChoreographyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SagaChoreographyApplication.class, args);
+    }
+
+}
